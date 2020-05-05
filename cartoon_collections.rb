b7baee7a462d622dd1c07f3be5feb6b68f856b69
Luke Chen <luke.chen.2010@gmail.com>
2020-05-05 15:33:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(planeteers)
   return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
